1bf49f472848959a84512d77668e94ec365a7d6a
kelsie_b <kelsie.p.brown@gmail.com>
2025-02-22 18:53:04 -0500

    Move code from cart.js and product-detail.js to main.js

--- audiophile/js/main.js
+++ audiophile/js/main.js
@@ -11,3 +11,56 @@ const navMenu = function () {
     });
 };
 navMenu();
+
+// Product details js page
+
+const subButton = document.querySelector('.minus');
+const addButton = document.querySelector('.plus');
+const displayNum = document.querySelector('.num');
+const backButton = document.querySelector('.go-back');
+let count = 1;
+
+// Item counter event for shopping cart
+
+subButton.addEventListener('click', () => {
+    if (count != 1) {
+        count--;
+        displayNum.innerHTML = count;
+    }
+});
+
+addButton.addEventListener('click', () => {
+    if (count < 1) {
+        count++;
+        displayNum.innerHTML = count;
+    } else {
+        count++;
+        displayNum.innerHTML = count;
+    }
+});
+
+displayNum.innerHTML = count;
+
+backButton.addEventListener("click", () => {
+    history.back();
+});
+
+// Shopping cart js page
+
+const cartButton = document.querySelector('.cart');
+const modal = document.querySelector('.modal-cart');
+
+// Hides cart modal
+// modal.classList.add('hide');
+
+// Toggles cart modal
+// const cartModal = function () {
+//     cartButton.addEventListener("click", () => {
+//         modal.classList.toggle('hide');
+//     });
+// };
+// cartModal();
+
+/*
+    Fix overlay to display behind cart modal not just beneath it
+*/
